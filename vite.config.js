9cc7b983dfd07dd919496af1a08e2449e6055681
Delunsss2345 <huypham1459@gmail.com>
2025-09-12 09:54:48 +0700

    up

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
-// https://vite.dev/config/
-export default defineConfig({
+
+export default defineConfig(({ command }) => ({
   plugins: [react(), tailwindcss()],
+  base: command === "build" ? "/f8-zoom-day37/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // alias @ trỏ vào thư mục src
     },
   },
-});
+}));
